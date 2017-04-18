2261eb679fbb6c5a8efaa3001e78029404deeaca
Adriano Freitas <archl0rd@adrianofreitas.me>
2017-04-18 17:48:45 -0400

    firewall editado

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-#
-# FIREWALL
-  # Autor: Adriano Freitas
-  # Modificado: 19/11/2016
+# Autor: Adriano Freitas <https://adrianofreitas.me>
 #
 
 function entrada(){
@@ -13,21 +10,18 @@ function entrada(){
   iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
   # Ignorar Ping (Opcional)
-  #iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
-  #iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+  iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+  iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 
   # Grava um registro no LOG quando recebe um pacote invalido
   iptables -A INPUT -m state --state INVALID -j LOG --log-prefix " FIREWALL: PACOTE INVALIDO "
 
   # Grava um registro no LOG quando recebe um pacote cujo destino e a porta 22
   # E libera a porta 22 (SSH)
-  iptables -A INPUT -p tcp --dport 22 -j LOG --log-prefix " FIREWALL: CONEXAO SSH "
+  iptables -A INPUT -p tcp --dport 22 -j LOG --log-prefix " FIREWALL: PORTA 22 "
   iptables -I INPUT -p tcp --dport 22 -m state --state NEW -m recent --update --seconds 360 --hitcount 3 -j DROP
   iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-  # Grava um registro no LOG
-  # E libera a porta 21 (FTP)
-  #iptables -A INPUT -p tcp --dport 21 -j LOG --log-prefix " FIREWALL: CONEXAO FTP "
-  #iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+
 
   # Grava um registro no LOG
   # E libera a porta 80 (HTTP)
@@ -36,13 +30,13 @@ function entrada(){
 
   # Grava um registro no LOG
   # E libera a porta 443 (HTTPS)
-  iptables -A INPUT -p tcp --dport 443 -j LOG --log-prefix " FIREWALL: CONEXAO HTTPS "
+  iptables -A INPUT -p tcp --dport 443 -j LOG --log-prefix " FIREWALL: PORTA 443 "
+  iptables -I INPUT -p tcp --dport 443 -m state --state NEW -m recent --update --seconds 360 --hitcount 3 -j DROP
   iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
 
   #BLOQUEAR O RESTANTE DAS PORTAS
   iptables -A INPUT -j DROP
-  #iptables -A INPUT -j LOG --log-prefix " FIREWALL "
 }
 
 function saida(){
