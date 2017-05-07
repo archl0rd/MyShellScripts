d48faa7b9b09ff428c049609af34e722e2f8d871
Adriano Freitas <archl0rd@adrianofreitas.me>
2017-05-07 08:21:29 +0000

    parâmetro N adicionado

--- shredR/shredR.sh
+++ shredR/shredR.sh
@@ -3,60 +3,45 @@
 # Modificado: 27/11/2016
 
 DIR=NULL
-N=100
+N=NULL
 function erase(){
   find $DIR -depth -type f -exec shred -zvu -n $N '{}' \;
   rm -R $DIR
 }
 
 function help() {
-  echo "Shred-R 0.1 - 2016 Adriano Freitas"
+  clear
+  echo "Shred-R 0.1 - 2017 Adriano Freitas"
   echo -e "\nOpções obrigatórias:"
-  echo -e "	\t-d <dir>\tCaminho para o diretório a ser excluído."
+  echo -e "	\t<dir>\tCaminho para o diretório a ser excluído."
   echo -e "\nOpções opcionais:"
-	echo -e "	\t-n <num>\tNúmero de vezes que os arquivos serão sobrescritos. (default: 10)"
-	echo -e "	\t-h\t\tExibir ajuda.\n"
-  echo -e "\nExemplos de uso: \nsh shred-R.sh -d DIR/"
-  echo -e "sh shred-R.sh -d DIR/ -n 20"
-  echo -e "sh shred-R.sh -d /home/user/DIR/ -n 30"
+	echo -e "	\t<num>\tNúmero de vezes que os arquivos serão sobrescritos. (default: 10)"
+	echo -e "	\t--help\t\tExibir ajuda.\n"
+  echo -e "\nExemplos de uso: \n"
+  echo -e "sh shredR.sh PATH/TO/DIR/"
+  echo -e "sh shredR.sh PATH/TO/DIR/ 20"
+  echo -e "sh shredR.sh PATH/TO/DIR/ 30"
   echo
+  echo -e "'sh shred-R.sh --help'\t para ajuda."
 }
 
 function main() {
-  while [ -n "$1" ]; do
-    case "$1" in
-      -d)
-      if [ -d "$2" ]; then
-        DIR=$2
-      else
-        echo "Diretório inexistente ou inválido!"
-      fi
-      break
-      ;;
+  if [ -d "$1" ]; then
+    DIR=$1
+  else
+    echo "Diretório inexistente ou inválido!"
+  fi
 
-      -n)
-      if let $2 2>/dev/null; then
-        N=$2
-      fi
-      break
-      ;;
-      -h | --help)
-      help
-      ;;
-      -*)
-      help
-      break
-      ;;
-    esac
-    shift
-  done
+  if let $2 2>/dev/null; then
+    N=$2
+  else
+    N=100
+  fi
 
   if [ -d "$DIR" ]; then
     erase
-    #echo $DIR
-    echo $N
   else
-    echo -e "'sh shred-R.sh --help' para ajuda."
+    help
   fi
 }
 
